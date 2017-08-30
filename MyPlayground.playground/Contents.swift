9a4ecad4c7f679fe93c7a8c637e49f78b2545d58
Lingzt <asklingtoby@gmail.com>
2017-08-30 09:14:50 -0400

    use mirror to get all the value of the properties and place it into an array

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+
+
+
+class DrivingTip {
+    
+    var name: String?
+    var type: String?
+}
+
+var tip = DrivingTip()
+
+tip.name = "Pika Pika"
+tip.type = "electricity"
+
+
+var descriptions = [String]()
+let drivingTipProperties = Mirror(reflecting: tip).children
+
+
+//descriptions = drivingTipProperties.map{$0.value as? String}.flatMap { $0 }
+
+for property in drivingTipProperties {
+    descriptions.append(property.value as! String)
+}
+
+descriptions
+
+
+
